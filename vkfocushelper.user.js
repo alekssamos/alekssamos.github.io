7524b4c363fcdda5e1b1e62c3c1d48e14c77db10
alekssamos <aleks-samos@yandex.ru>
2020-05-04 14:34:17 +0300

    my_commit1

--- vkfocushelper.user.js
+++ vkfocushelper.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name     VK accessibility helper
-// @version  1.2
+// @version  1.3
 // @grant    none
 // @include     https://vk.com/*
 // ==/UserScript==
 
 window.setInterval(function(){
+	try { document.querySelector('div#queue_transport_wrap').setAttribute('aria-hidden', 'true'); } catch(er){}
 	var el, els=document.querySelectorAll('div#wl_post, div#pv_box, div.ap_layer_wrap div.ap_layer div.ap_layer__content, div[class*="popup_box"][tabindex="0"], div#box_layer_wrap.scroll_fix_wrap.fixed, div.article_layer _article_layer');
 	for(var i=0; i<els.length; i++){
 		el=els[i];
