3550896f67b3567e398f38c2518a5b8e7285fa64
alekssamos <aleks-samos@yandex.ru>
2020-05-04 14:27:18 +0300

    my_commit1

--- vkfocushelper.user.js
+++ vkfocushelper.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name     VK accessibility helper
-// @version  1
+// @version  1.2
 // @grant    none
 // @include     https://vk.com/*
 // ==/UserScript==
 
 window.setInterval(function(){
-	var el, els=document.querySelectorAll('div.ap_layer_wrap div.ap_layer div.ap_layer__content, div[class*="popup_box"][tabindex="0"], div#box_layer_wrap.scroll_fix_wrap.fixed, div.article_layer _article_layer');
+	var el, els=document.querySelectorAll('div#wl_post, div#pv_box, div.ap_layer_wrap div.ap_layer div.ap_layer__content, div[class*="popup_box"][tabindex="0"], div#box_layer_wrap.scroll_fix_wrap.fixed, div.article_layer _article_layer');
 	for(var i=0; i<els.length; i++){
 		el=els[i];
 		if(!!el.parentNode.querySelector('*:focus')) continue;
