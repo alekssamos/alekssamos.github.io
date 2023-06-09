7f4198b4805866a1101ec1a2bc1ab4766c005304
Alexey <aleks-samos@yandex.ru>
2023-06-09 14:00:20 +0300

    Updated  userscript aria hidden

--- enableinaccessabledialogs.user.js
+++ enableinaccessabledialogs.user.js
@@ -2,12 +2,14 @@
 // @name         fix aria hidden for display block dialog
 // @namespace    http://tampermonkey.net/
 // @homepage    https://alekssamos.github.io/eid.html
-// @version      0.5
+// @version      0.6
 // @description  aria-hidden true but style display block, enable this dialogs
 // @author       alekssamos
-// @match        https://*
+// @include        https://*.*
+// @include        https://*.*.*
+// @include        http://*.*
+// @include        http://*.*.*
 // @run-at document-start
-// @icon         https://www.google.com/s2/favicons?domain=moneyman.ru
 // @grant        none
 // ==/UserScript==
 
@@ -16,7 +18,7 @@
 
     // Your code here...
     window.setInterval(function(){
-        let elems = document.querySelectorAll('div[role="dialog"][aria-hidden="true"]');
+        let elems = document.querySelectorAll('*[aria-hidden="true"], *[aria-hidden="true"]');
         for (let i=0; i < elems.length; i++) {
             //if(elems[i].getAttribute("style").indexOf("block")==-1) continue;
             elems[i].removeAttribute("aria-hidden");
