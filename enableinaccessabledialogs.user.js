711b65487f22b700bc2f9a34911e376be1b3d99a
Alexey <aleks-samos@yandex.ru>
2023-06-09 14:15:36 +0300

    Userscript aria-hidden: Slightly changed the description and added VK to the exclude

--- enableinaccessabledialogs.user.js
+++ enableinaccessabledialogs.user.js
@@ -2,8 +2,8 @@
 // @name         fix aria hidden for display block dialog
 // @namespace    http://tampermonkey.net/
 // @homepage    https://alekssamos.github.io/eid.html
-// @version      0.6
-// @description  aria-hidden true but style display block, enable this dialogs
+// @version      0.7
+// @description  aria-hidden true, enable this dialogs
 // @author       alekssamos
 // @include        https://*.*
 // @include        https://*.*.*
@@ -18,6 +18,10 @@
 
     // Your code here...
     window.setInterval(function(){
+        if (document.domain == "vk.com") {
+            /* для VK у меня просто есть отдельный скрипт, скрывающий текстовые поля и фреймы в начале */
+            return false;
+        }
         let elems = document.querySelectorAll('*[aria-hidden="true"], *[aria-hidden="true"]');
         for (let i=0; i < elems.length; i++) {
             //if(elems[i].getAttribute("style").indexOf("block")==-1) continue;
