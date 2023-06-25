0edde475b33d088fb7573f5bba5a6cab4ef0c2cd
Alexey <aleks-samos@yandex.ru>
2023-06-25 11:02:09 +0300

    my_commit1

---
+++ boo.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         boo.world clickable elements accessibility
+// @namespace    http://tampermonkey.net/
+// @homepage    https://alekssamos.github.io/boo.html
+// @version      0.1
+// @description  There is an empty label in the pressed elements and even the left-right arrows on the computer cannot be focused and pressed, and even more so on smartphones
+// @author       alekssamos
+// @include        https://boo.world/*
+// @run-at document-start
+// @grant        none
+// ==/UserScript==
+
+window.setInterval(()=>{
+	document.querySelectorAll("div.clickable, img.clickable").forEach(elem=>{
+		if(elem.getAttribute("role") != "button") {
+			with(elem) {
+				setAttribute("role", "button");
+				setAttribute("tabindex", "0");
+				if(innerHTML == "" && getAttribute("onclick").length > 8) {
+					setAttribute("aria-label", getAttribute("onclick"));
+				}
+			}
+		}
+	});
+}, 500);
