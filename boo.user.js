4db139066f2bc15bb3e43b708c5b815bb7e6c8b7
Alexey <aleks-samos@yandex.ru>
2023-07-01 09:55:54 +0300

    my_commit1

--- boo.user.js
+++ boo.user.js
@@ -2,7 +2,7 @@
 // @name         boo.world clickable elements accessibility
 // @namespace    http://tampermonkey.net/
 // @homepage    https://alekssamos.github.io/boo.html
-// @version      0.12
+// @version      0.13
 // @description  There is an empty label in the pressed elements and even the left-right arrows on the computer cannot be focused and pressed, and even more so on smartphones. And also makes the states and roles of elements available.
 // @author       alekssamos
 // @include        https://boo.world/*
@@ -15,12 +15,16 @@ var passingElements = function() {
 	if(msgl && msgl.getAttribute('aria-live') != 'polite') {
 		msgl.setAttribute('aria-live', 'polite');
 	}
+	
 	document.querySelectorAll(".clickable, #moreOptionsIcons").forEach(elem=>{
-		if(elem.classList.contains("bubble") && elem.classList.contains("clickable")) {
-			elem.setAttribute("aria-pressed", elem.classList.contains("interest-highlighted")?"true":"false");
-		}
-		if(elem.getAttribute("role") != "button") {
-			with(elem) {
+		with(elem) {
+			if(classList.contains("bubble")) {
+				setAttribute("aria-pressed", classList.contains("interest-highlighted")?"true":"false");
+			}
+			if(getAttribute("onclick").indexOf("likeContent")!=-1) {
+				setAttribute("aria-pressed", (getAttribute("onclick").indexOf("true")!=-1)?"true":"false");
+			}
+			if(getAttribute("role") != "button") {
 				setAttribute("aria-live", "off");
 				setAttribute("role", "button");
 				setAttribute("tabindex", "0");
