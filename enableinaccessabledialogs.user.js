69ce8cf6e7bc7014700f492512885ccb0efbae6f
Alexey <aleks-samos@yandex.ru>
2023-08-06 14:46:18 +0300

    my_commit1

--- enableinaccessabledialogs.user.js
+++ enableinaccessabledialogs.user.js
@@ -2,7 +2,7 @@
 // @name         fix aria hidden for display block dialog
 // @namespace    http://tampermonkey.net/
 // @homepage    https://alekssamos.github.io/eid.html
-// @version      0.7
+// @version      0.8
 // @description  aria-hidden true, enable this dialogs
 // @author       alekssamos
 // @include        https://*.*
@@ -16,16 +16,14 @@
 (function() {
     'use strict';
 
-    // Your code here...
     window.setInterval(function(){
         if (document.domain == "vk.com") {
             /* для VK у меня просто есть отдельный скрипт, скрывающий текстовые поля и фреймы в начале */
             return false;
         }
-        let elems = document.querySelectorAll('*[aria-hidden="true"], *[aria-hidden="true"]');
-        for (let i=0; i < elems.length; i++) {
-            //if(elems[i].getAttribute("style").indexOf("block")==-1) continue;
-            elems[i].removeAttribute("aria-hidden");
-        }
-    }, 1000);
+
+        document.querySelectorAll('*[aria-hidden="true"]').forEach(elem=>{
+            elem.removeAttribute("aria-hidden");
+        });
+    }, 500);
 })();
