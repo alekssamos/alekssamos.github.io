25c942d6e8592e6052d011b45331ab887cae58c3
alkssamos <aleks-samos@yandex.ru>
2025-01-17 14:18:04 +0300

    my_commit1

--- enableinaccessabledialogs.user.js
+++ enableinaccessabledialogs.user.js
@@ -2,16 +2,14 @@
 // @name         fix aria hidden for display block dialog
 // @namespace    http://tampermonkey.net/
 // @homepage    https://alekssamos.github.io/eid.html
-// @version      0.23
+// @version      0.24
 // @description  aria-hidden true, enable this dialogs
 // @author       alekssamos
-// @include        https://*.nalog.ru/*
-// @include        https://*.*.nalog.ru/*
-// @include        https://*.*.*.nalog.ru/*
-// @include        https://rutube.ru/*
-// @include        https://*.rutube.ru/*
-// @include        https://*.icloud.com/*
-// @include        *://alekssamosbt.ru/*
+// @include        *://*nalog.ru/*
+// @include        https://*rutube.ru/*
+// @include        https://*apple.com/*
+// @include        https://*icloud.com/*
+// @include        *://*alekssamosbt.ru/*
 // @run-at document-start
 // @grant        none
 // ==/UserScript==
