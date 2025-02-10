15fd56a90ca1c21515d582497a631e30a9f23cf2
alkssamos <aleks-samos@yandex.ru>
2025-02-10 18:45:51 +0300

    my_commit1

--- a11y_improver.user.js
+++ a11y_improver.user.js
@@ -2,7 +2,7 @@
 // @name         various accessibility improvements for different sites
 // @namespace    http://tampermonkey.net/
 // @homepage    https://alekssamos.github.io/a11y.html
-// @version      0.9
+// @version      0.10
 // @description  Making accessable checkboxes, buttons, and other elements on different sites.
 // @author       alekssamos
 // @include        *://*habr*/*
@@ -23,66 +23,8 @@
 */
 
 (function() {
-/*<vdsina>*/
-function set_menu_clickable_from_keyboard(elem){
-	elem.setAttribute("tabindex", "0");
-	elem.setAttribute("role", "menuitemradio");
-	if(elem.parentNode.getAttribute("role")!="menu") {
-		elem.parentNode.setAttribute("role", "menu");
-		elem.parentNode.setAttribute("tabindex", "-1");
-	}
-}
 
-if(document.domain.indexOf("vdsina")!=-1){
-        document.querySelectorAll('span.btn').forEach(elem=>{
-            if(elem.getAttribute("role")=="button") return true;
-            elem.setAttribute("role", "button");
-            elem.setAttribute("tabindex", "0");
-        });
-        
-    window.setInterval(function(){
-document.querySelectorAll('div[class*="disabled"]').forEach(elem=>{
-	with(elem){
-		let d = getAttribute("aria-disabled");
-		if(d!="true" && d!="false") {
-			setAttribute("aria-disabled", "true");
-			set_menu_clickable_from_keyboard(elem);
-			parentNode.parentNode.addEventListener("onclick", event=>{
-				window.setTimeout(()=>{ setAttribute("aria-disabled", classList.contains("disabled")?"true":"false"); },100);
-			});
-		}
-	}
-});
-document.querySelectorAll('div.selected, div[class*="-list-item"]').forEach(elem=>{
-	with(elem){
-		if(getAttribute("class").indexOf("list-item-down")!=-1) return;
-		if(getAttribute("class").indexOf("twi")!=-1) return;
-		if(getAttribute("class").indexOf("list-item-up")!=-1) return;
-		if(classList.contains("groups")){
-			if(getAttribute("tabindex")!="0"){
-				parentNode.parentNode.addEventListener("onclick", event=>{
-					window.setTimeout(()=>{ setAttribute("aria-expanded", classList.contains("selected")?"true":"false"); }, 100);
-				});
-			}
-			setAttribute("tabindex", "0");
-			setAttribute("role", "button");
-			setAttribute("aria-expanded", classList.contains("selected")?"true":"false");
-			return;
-		}
-		let d = getAttribute("aria-checked");
-		if(d!="true" && d!="false") {
-			setAttribute("aria-checked", classList.contains("selected")?"true":"false");
-			set_menu_clickable_from_keyboard(elem);
-			parentNode.parentNode.addEventListener("onclick", event=>{
-				window.setTimeout(()=>{ setAttribute("aria-checked", classList.contains("selected")?"true":"false"); }, 100);
-			});
-		}
-	}
-});
-}
-}
-},700);
-/*</vdsina>*/
+
     window.setInterval(function(){
         document.querySelectorAll("label").forEach(el=>{
             let checkbox_id = el.getAttribute('for');
