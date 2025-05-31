5a05ba610f2672e84e03cd6f61c4e76ac13e3d88
alkssamos <aleks-samos@yandex.ru>
2025-05-31 19:39:25 +0300

    my_commit1

--- a11y_improver.user.js
+++ a11y_improver.user.js
@@ -2,7 +2,7 @@
 // @name         various accessibility improvements for different sites
 // @namespace    http://tampermonkey.net/
 // @homepage    https://alekssamos.github.io/a11y.html
-// @version      0.18
+// @version      0.19
 // @description  Making accessable checkboxes, buttons, and other elements on different sites.
 // @author       alekssamos
 // @include        *://*habr*/*
@@ -78,6 +78,10 @@ let ctrl_labels = {
         }
         document.querySelectorAll("button[data-hint], img[data-hint]").forEach(function(el){
             el.setAttribute("aria-label", el.getAttribute("data-hint"));
+        });
+        document.querySelectorAll('div[class="data-original-title"]').forEach(elem=>{
+            elem.setAttribute("tabindex", "0");
+            elem.setAttribute("aria-label", elem.getAttribute("data-original-title"));
         });
         document.querySelectorAll("label").forEach(el=>{
             let checkbox_id = el.getAttribute('for');
